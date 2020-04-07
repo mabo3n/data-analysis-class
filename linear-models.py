a50a65ba3e8fcf2ac5b6c96f8c7d57291a5daceb
Marcel <dolly.marcel@gmail.com>
2020-04-06 21:06:39 -0300

    Add linear-models.py
    Reading the data and scatter plotting the age x weights

--- linear-models.py
+++ linear-models.py
@@ -1,3 +1,26 @@
 #!/bin/python
 
-# Hello!
+# Importa bibliotecas necessarias
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as st
+
+# Baixa e guarda os dados disponibilizados pelo professor em um dataframe
+url = 'http://dropbox.com/s/jv9ukzk58vck3oh/Gestacao.csv?dl=1'
+data = pd.read_csv(url, sep=';', decimal=',')
+
+# Exibe os primeiros 10 registros
+data.head(10)
+
+# Exibe um resumo de cada coluna
+data.describe()
+
+x = data["Age"]
+y = data["Weight"]
+s = data["Births"]  # sizes
+
+plt.xlabel("Idade gestacional (em semanas)")
+plt.ylabel("Media de peso dos bebes")
+plt.scatter(x, y, s)
+plt.show()
